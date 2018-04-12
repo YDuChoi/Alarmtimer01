0ff66d44bfc8274da373855948936ab6fc1ba4c5
youngju <asdadssad@naver.coim>
2018-04-12 15:43:00 +0900

    time

--- Alarmtimer/ViewController.swift
+++ Alarmtimer/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    var myTimer = Timer()
+    var count = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyy-MM-dd HH:mm:ss"
+        timeLabel.text = formatter.string(from: myDatePicker.date)
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()
+            
+        })
     }
-
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyy-MM-dd HH:mm:ss"
+        currentTimeLabel.text = formatter.string(from: date)
+        if currentTimeLabel.text == timeLabel.text {
+            view.backgroundColor = UIColor.red
+            count = false
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func changeDatePicker(_ sender: Any) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyy-MM-dd HH:mm:ss"
+        timeLabel.text = formatter.string(from: myDatePicker.date)
+    }
+    
 
-
+    @IBAction func btn(_ sender: Any) {
+        if count == false {
+            view.backgroundColor = UIColor.white
+            count = true
+        }
+    }
+    
 }
 
